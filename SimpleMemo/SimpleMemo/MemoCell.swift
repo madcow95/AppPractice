5c382a9036f3b99559b7e54ef8b146c9367f9ac6
madcow95 <lobasketve@naver.com>
2023-05-28 23:16:41 +0900

    ViewCell / Label 생성

---
+++ SimpleMemo/SimpleMemo/MemoCell.swift
@@ -0,0 +1,21 @@
+//
+//  MemoCell.swift
+//  SimpleMemo
+//
+//  Created by ChoiKwangWoo on 2023/05/28.
+//
+
+import UIKit
+
+class MemoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memoWriteDateLabel: UILabel!
+    @IBOutlet weak var memeNumberLabel: UILabel!
+    @IBOutlet weak var memoSubjectLabel: UILabel!
+    @IBOutlet weak var memoContentLabel: UILabel!
+    
+//    memoWriteDateLabel.text = "2023.05.28"
+//    memeNumberLabel.text = "1"
+//    memoSubjectLabel.text = "제목"
+//    memoContentLabel.text = "내용"
+}
