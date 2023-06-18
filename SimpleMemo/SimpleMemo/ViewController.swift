26514ca2a4f6b756aeb66e1f82bf1f3b12929e2f
madcow95 <lobasketve@naver.com>
2023-06-18 22:21:14 +0900

    .

--- SimpleMemo/SimpleMemo/ViewController.swift
+++ SimpleMemo/SimpleMemo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("view did load test")
+        // 하,, 처음부터 다시해야겠네
         // Do any additional setup after loading the view.
     }
 
